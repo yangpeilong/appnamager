b592741c3bdce708a86964fe46103cf48b524375
yangyeah <yangpeilong@qding.me>
2019-09-02 16:39:30 +0800

    tijiao

--- gitLibrary/src/main/java/com/example/gitlibrary/AppManager.kt
+++ gitLibrary/src/main/java/com/example/gitlibrary/AppManager.kt
@@ -1,9 +1,19 @@
 package com.example.gitlibrary
 
+import android.app.Application
+
 /**
  *  Created by yangyeah on 2019/9/2
  *
  *  describe:
  */
 object AppManager {
+
+    fun init(app: Application) {
+        
+    }
+
+    fun set() {
+        
+    }
 }
